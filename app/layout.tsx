e1515d0e5879e6984334358becb53e1782ef5a0f
abthalhafizh <abthalhafizh@gmail.com>
2026-01-12 21:06:51 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Peta Dinamis - Bulusidokare",
-  description: "Peta Dinamis - Bulusidokare",
+  title: "Peta Dinamis - kemasan",
+  description: "Peta Dinamis - kemasan",
 };
 
 export default function RootLayout({
